a40715c09451fa1f42709489333da272dcb6c4f8
Ryan Setaruddin <coolry1998@gmail.com>
2023-09-12 16:49:57 -0400

    fixed the certain characters function, and modified the countsOfListedCharacters and countsOflistedWords functions.

--- Text/text_manipulation.py
+++ Text/text_manipulation.py
@@ -1,8 +1,8 @@
 # This module will manipulates text
 from text_statistics import *
 
-def capitalizeFirstLetterOfEverySentences():
+def capitalizeFirstLetterOfEverySentences(text: str)-> str:
 	pass
 
-def replacementSetOfWords():
+def replacementSetOfWords(text: str)-> str:
 	pass
--- Text/text_statistics.py
+++ Text/text_statistics.py
@@ -8,7 +8,9 @@ import pickle
 filename = "data.text"
 filename2 = "data.json"
 
-# this function will gets the total of paragraph
+"""
+	This function is to get the total numbers of paragraphs in text
+"""
 def getTotalParagraphsInText(text: str)-> int:
 	num_of_paragraph = 0
 
@@ -22,6 +24,9 @@ def getTotalParagraphsInText(text: str)-> int:
 			num_of_paragraph += 1
 	return num_of_paragraph
 
+"""
+	This function is to get the total numbers of paragraphs in file
+"""
 def getTotalParagraphsInFile(file: str)-> int:
 	num_of_paragraph = 0
 	try:
@@ -34,6 +39,9 @@ def getTotalParagraphsInFile(file: str)-> int:
 		msg = "The file " + filename + " does not exist."
 	return num_of_paragraph
 
+"""
+	This function is to get total numbers of characters in text
+"""
 def getTotalCharactersInText(text: str)-> int:
 	num_of_characters = 0
 
@@ -43,18 +51,24 @@ def getTotalCharactersInText(text: str)-> int:
 	num_of_characters = len(text)
 	return num_of_characters
 
+"""
+	This function is to get the total numbers of characters in file
+"""
 def getTotalCharactersInFile(file: str)-> int:
 	num_of_characters = 0
 	try:
 		with open(filename, "r+") as file:
 			content = file.read()
-		num_of_characters = getTotalCharactersInText(content)
+		num_of_characters = getTotalCharactersInText(content) 
 	except FileNotFoundError as fe:
 		raise fe
 	else:
 		msg = "The file " + filename + " does not exist."
 	return num_of_characters
 
+"""
+	This function is to get the total numbers of words in text
+"""
 def getTotalWordsInText(text: str)-> int:
 	num_of_words = 0
 	
@@ -68,6 +82,9 @@ def getTotalWordsInText(text: str)-> int:
 			num_of_words += 1
 	return num_of_words
 
+"""
+	This function is to get the total numbers of words in file
+"""
 def getTotalWordsInFile(file: str)-> int:
 	num_of_words = 0
 	try:
@@ -80,6 +97,9 @@ def getTotalWordsInFile(file: str)-> int:
 		msg = "The file " + filename + " does not exist."
 	return num_of_words
 
+"""
+	This function is to get the total numbers of sentences in text
+"""
 def getTotalSentencesInText(text: str)-> int:
 	num_of_sentences = 0
 
@@ -93,6 +113,9 @@ def getTotalSentencesInText(text: str)-> int:
 			num_of_sentences += 1
 	return num_of_sentences
 
+"""
+	This function is get the total numbers of sentences in file
+"""
 def getTotalSentencesInFile(file: str)-> int:
 	num_of_sentences = 0
 	try:
@@ -105,60 +128,90 @@ def getTotalSentencesInFile(file: str)-> int:
 		msg = "The file " + filename + " does not exist."
 	return num_of_sentences
 
+"""
+	This function is to get the total numbers of certain characters.
+"""
 def getTotalCountsOfCertainCharacter(certainCharacter: str, text: str)-> int:
 	num_of_certain_characters = 0
 
-	if certainCharacter is None and text is None:
+	if certainCharacter is None or text is None:
 		return num_of_certain_characters
-		
-	certainCharacter = getTotalCharactersInText(text)
 
-	for cc in certainCharacter:
-		if cc != "":
+	for cc in text:
+		if cc == certainCharacter:
 			num_of_certain_characters += 1
 	return num_of_certain_characters
 
-def getTotalCountsOfListedCharacters(listedCharacters: list[str], text: str)-> int:
+"""
+	This function is to get the total numbers of listed characters.
+	In this function, we are allow to reuse other functions if its necessary
+"""
+def getTotalCountsOfListedCharacters(listedCharacters: list[str], text: str)-> dict[str, int]:
+	"""
+		listedCharacters - A list of the charcters we are looking for
+		Return the count of each character, in a dict format.
+
+		Example: Find all a's and b's in a text.
+		text = "This is a text. It does have a b in it, boy oh boy!"
+		result = getTotalCountsOfListedCharacters(["a", "b""], text)
+		{
+			"a": 3,
+			"b": 3
+		}
+	"""
 	listed_character_dict = {}
-	totalCountsOfListedCharacters = 0
 
-	if listed_character_dict is None and text is None:
-		return totalCountsOfListedCharacters
-
-	listedCharacters = getTotalCharactersInText(text)
-	listed_character_dict.append(listedCharacters)
+	if listed_character_dict is None or text is None:
+		return
 
 	for lc in listedCharacters:
-		if lc != "":
-			totalCountsOfListedCharacters += 1
-			return dict.items(listed_character_dict)
-	return totalCountsOfListedCharacters
+		outcome = getTotalCountsOfCertainCharacter(lc, text)
+		listed_character_dict[lc] = outcome
+	return listed_character_dict
 
-def getTotalCountsOfListedWords(listedWords: list[str], text: str)-> int: 
+"""
+	This function is to get the total numbers of listed words.
+	In this function, we are allow to reuse other functions if its necessary
+"""
+def getTotalCountsOfListedWords(listedWords: list[str], text: str)-> dict[str, int]: 
 	listed_words_dict = {}
-	totalCountsOfListedWords = 0
 
-	if listedWords is None and text is None:
-		return totalCountsOfListedWords
-
-	listedWords = getTotalWordsInText(text)
-	listed_words_dict.append(listedWords)
+	if listedWords is None or text is None:
+		return
 
 	for lw in listedWords:
-		if lw != "":
-			totalCountsOfListedWords += 1
-			return dict.items(listed_words_dict)
-	return totalCountsOfListedWords
- 
+		outcome = getTotalWordsInText(lw)
+		listed_words_dict[lw] = outcome
+	return listed_words_dict
+
+"""
+	This function is to the average length of words
+"""
 def getAverageLengthOfWords(text: str)-> int:
-	average = 0
+	averageLengthOfWords = 0
 
 	lengthOfWords = len(getTotalWordsInText(text))
 
-	return average
+	averageLengthOfWords += lengthOfWords
 
+	return averageLengthOfWords
+
+"""
+	This function is to get the average length of sentences
+"""
 def getAverageLengthOfSentences(text: str)-> int:
-	pass
+	averageLengthOfSentences = 0
 
+	lengthOfsentences = len(getTotalWordsInText(text))
+
+	averageLengthOfSentences += lengthOfWords
+
+	return averageLengthOfSentences
+
+"""
+	This function is to get the average numbers of each sentences in in each paragarphas
+"""
 def getAverageNumbersOfSentencesPerParagraph(text: str)-> int:
-	pass
+	averageNumberOfSenetncesPerParagraph = 0
+
+	return averageNumberOfSenetncesPerParagraph
