cca73f7fcdab07fbd8a100ee78d51ff13e2dfbf8
Ryan Setaruddin <coolry1998@gmail.com>
2023-09-19 16:35:00 -0400

    modified replacementwords function.

--- src/Text/text_manipulation.py
+++ src/Text/text_manipulation.py
@@ -1,6 +1,5 @@
 # This module will manipulates text
 from text_statistics import *
-import re
 
 """
 This function is to capitalizes every first letters in every sentences
@@ -24,16 +23,16 @@ def capitalizeFirstLetterOfEverySentences(text: str)-> str:
 	This function is to replece set of words with other set of words
 """
 def replacementSetOfWords(wordReplacement: dict[str, str], text: str)-> str:
-	replaceWords = ""
+	new_word_dict = {} # Create a new empty dictionary to store the modified words
 
-	words = text.split(" ")
+	words = text.split(" ") # split the given stringg into list of words
 
-	for rw in words:
+	for rw in words: # Loop through each word in the list of words
 		# check if this word should be replace?
-		if :
-			pass
-
-		# replace old word with new word
-		text.replace()
-
-	return 
+		if rw == wordReplacement: # check if the current word is equal to the given word wordReplacement
+			new_word_dict.append(rw) # If the word is equal to wordReplacement, append it to new dictionary
+		else:
+			# replace old word with new word
+			new_word_dict.append() # If the word is not equal to wordReplacement, append the 
+	modified_str = " ".join(new_word_dict) # join the new list of words into a string with spaces between each word
+	return modified_str # Return the modified string
