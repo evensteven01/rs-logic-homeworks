f7d0506c746e3380b2e430a7533739be057da568
Ryan Setaruddin <coolry1998@gmail.com>
2023-10-25 19:02:11 -0400

    having trouble with the functions that has typerror: nonetype is not iterable. tried to do differnet way didn't work as tought. added new functions but has passes not implemented yet.

--- src/Basics/loops_hw1.py
+++ src/Basics/loops_hw1.py
@@ -1,12 +1,37 @@
-from utils import get_random_nums
-
+from src.Basics.utils import get_random_nums
 
 """
 	Loops Homework 1
 """ 
 
-lst = [i for i in get_random_nums()]
+def get_maximum(listOfNumbers: list[float]):
+	maximumNumber = None
 
+	for value in listOfNumbers:
+		if maximumNumber is None:
+			maximumNumber = value
+		elif maximumNumber < value:
+			maximumNumber = value
+	return maximumNumber
 
+def get_minimum(listOfNumbers: list[float]):
+	minimumNumber = None
 
+	for value in listOfNumbers:
+		if minimumNumber is None:
+			minimumNumber = value
+		elif minimumNumber > value:
+			minimumNumber = value
+	return minimumNumber
 
+def get_average(listOfNumbers: list[float]):
+	pass
+
+def get_closest(listOfNumbers: list[float], num: float):
+	pass
+
+def add_x(listOfNumbers: list[float], num: float):
+	pass
+
+def sub_x(listOfNumbers: list[float], num: float):
+	pass
