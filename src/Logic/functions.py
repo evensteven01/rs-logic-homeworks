9b8f22ed489fb7bd5a363f3ddc852ce32bda2518
Ryan Setaruddin <coolry1998@gmail.com>
2023-12-10 18:04:50 -0500

    completing some the functions, others struggling to understand. Also not every python libaray module may or may not be compataible due to the requirements and severes or depends on if the internet speed and or on/off.

--- src/Logic/functions.py
+++ src/Logic/functions.py
@@ -1,5 +1,7 @@
 import copy
 
+from PyDictionary import PyDictionary
+
 def _register(*args, **kwargs):
     """ This is just for me to use in unit testing """
 
@@ -22,24 +24,53 @@ def example(*args, **kwargs):
 def _cregister(*args, **kwargs):
     pass
 
-def return_greater():
-    pass
+def return_greater(arg1, arg2):
 
-def _get_first_two_letters():
-    pass
+    obj1 = arg1
+    obj2 = arg2
 
-def combine_first_two():
-    pass
+    greater = lambda obj1, obj2: obj1 if obj1 > obj2 else obj2
 
-def positional_args():
-    pass
+    if isinstance(obj1, str) == str and isinstance(obj2, str) == str:
+        pass
+    else:
+        pass
+
+def _get_first_two_letters(arg1: str, arg2: str):
+    
+    for i in arg1:
+        for j in arg2:
+            return i + j
+
+def combine_first_two(arg1: str, arg2: str):
+    return _get_first_two_letters(arg1, arg2)
+
+def positional_args(arg1: str, arg2: str, arg3: str):
+
+    string1 = arg1
+    string2 = arg2
+    string3 = arg3
+
+    _register(string1, string2, string3)
+    _register(string3, string2, string1)
 
 def sum_numbers():
     pass
 
 
-def try_kwargs():
-    pass
+def try_kwargs(string: str, integer: int, flt: float, boolean: bool, lst: list):
+
+    str_arg = string
+    int_arg = integer
+    float_arg = flt
+    bool_arg = boolean
+    list_arg = lst
+
+    _register(str_arg, int_arg, float_arg, bool_arg, list_arg)
+    _register(list_arg, bool_arg, float_arg, int_arg, str_arg)
+    _register(str_arg, list_arg, int_arg, bool_arg, float_arg)
 
 def sum_if_param_name_starts_with_a():
-    pass
+    
+    if :
+        pass
