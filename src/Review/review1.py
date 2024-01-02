786292da27cc4f81601b24f1ec6bf550a6bebc81
Ryan Setaruddin <coolry1998@gmail.com>
2024-01-02 18:23:27 -0500

    some of functions that i may not grasp and having hard time with them.

--- src/Review/review1.py
+++ src/Review/review1.py
@@ -1,4 +1,6 @@
-import math
+ import math
+
+ from decimal import *
 
 def fuel_cost(liters: float, pricePerLiter: float):
 
@@ -22,26 +24,55 @@ def ints_between(start: int, end: int):
     return array
 
 def mpg21p100km(mpg: float):
-    pass
+    
+    mile = 1.609344
+
+    gallon = 3.785411784
+
+    kms_per_mpg = (100 / mpg)
+
+    kms_per_gallon = (kms_per_mpg * gallon)
+
+    miles_per_gallon = (kms_per_gallon / mile)
+
+    return miles_per_gallon
 
 def lp100km2mpg(lp100: float):
-    pass
+    
+    mile = 1.609344
+
+    gallon = 3.785411784
+
+    gallon_per_100miles = (100 / lp100)
+
+    gallon_per_100kms = (gallon_per_100miles / mile)
+
+    liters_per_100kms = (gallon_per_100kms * gallon)
+
+    return liters_per_100kms
+
 
 def sticky_calculator(operator: str, val1: int, val2: int):
+
+    val1 = int(input("Please enter your first number:_"))
+
+    val2 = int(input("Please enter your second number:_"))
+
+    operator = input("Please enter arithmetic operators such as (+, _ , *, /):_")
     
     if operator == "+":
         return result = (val1 * val2)
 
     elif operator == "/"
         return result = (val1 * val2)
-
+ 
     elif operator == "+":
         return result = (val1 + val2)
 
     elif operator == "-":
         return result = (val1 - val2)
 
-def global_estimate():
+def global_estimate(estimates):
     pass
 
 def add(*args):
